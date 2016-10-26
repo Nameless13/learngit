775bba291b76faa541a88e13c64d6d6338a79e81
Nameless13 <jfjfhajj@163.com>
2016-10-26 18:14:27 +0800

    none

---
+++ python/for.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+for letter in 'Python':
+  print '当前字母 :', letter
+
+
+fruits = ['banana','apple','mango']
+
+for fruit in fruits:
+  print '当前字母 :', fruit
+
+print "Good bye!"
---
+++ python/tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding:UTF-8 -*-
+
+tuple=('runoob',786,2.23,'john',70.2)
+tinytuple=(123,'john')
+
+print tuple
+print tuple[0]
+print tuple[1:3]
+print tuple[2:]
+print tinytuple * 2
+print tuple + tinytuple
