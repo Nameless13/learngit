4c98f202b7e0c512f885da7f692fe64c263f5513
Nameless13 <jfjfhajj@163.com>
2016-11-02 00:13:36 +0800

    nonthing

--- nodejs/https.js
+++ nodejs/https.js
@@ -1,1 +1,13 @@
-https.js
+var https = require('https')
+var fs = require('fs')
+
+var options= {
+	key : fs.readFileSync('ssh_key.pem'),
+	cert: fs.readFileSync('ssh_cert.pem')
+}
+
+https.creatServer(options,function(req,res){
+	res.writeHead(200)
+	res.end('Hello Immoc')
+})
+.listen(8090)
